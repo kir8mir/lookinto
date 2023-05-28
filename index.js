923fd9061a069041d9b7ec21c16ebd46e802d3a8
Cyrill Prue <49589547+kir8mir@users.noreply.github.com>
2023-05-28 16:46:49 +0300

    port

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const addToUserWord = require("./utils/addToUserWord");
 
 const app = new Koa();
 const router = Router();
-const port = 3330;
-const url = "89.40.2.236";
+const port = 80;
+const url = "89.40.2.236:80";
 
 bot.setWebHook(`${url}/bot`);
 
