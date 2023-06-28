218dfc0e5de87c6a8bcaeb301a711b7d4443eafd
Cyrill Prue <49589547+kir8mir@users.noreply.github.com>
2023-06-28 21:47:56 +0300

    g

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ bot.on("message", (msg) => {
     })();
   }
   if (text === "/site") {
-    const link = `https://localhost:3000/`;
+    const link = `http://localhost:3000/profile.html`;
     const message = `<a href="${link}">22</a>`;
     bot.sendMessage(chatId, message, { parse_mode: 'HTML' });
   }
