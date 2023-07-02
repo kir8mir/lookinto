d795c424706e6c6fdf379876a4de0472a7b429ac
Cyrill Prue <49589547+kir8mir@users.noreply.github.com>
2023-07-02 14:51:40 +0300

    cr

--- index.js
+++ index.js
@@ -33,15 +33,15 @@ const updateServer = async () => {
 
     let userState = userStates.get(userId);
 
-    if (!userState) {
-      // Если состояние пользователя не существует, создаем новый объект состояния
-      userState = {
-        isQueryProcessed: false,
-        rightAnswer: word.translations[0].title,
-        // Другие свойства состояния пользователя
-      };
-      userStates.set(userId, userState);
-    }
+      if (!userState) {
+        // Если состояние пользователя не существует, создаем новый объект состояния
+        userState = {
+          isQueryProcessed: false,
+          rightAnswer: word.translations[0].title,
+          // Другие свойства состояния пользователя
+        };
+        userStates.set(userId, userState);
+      }
 
     const answers = [
       word.translations[0].title,
@@ -73,55 +73,53 @@ const updateServer = async () => {
           clearTimeout(timeout);
         }, 60000);
       });
-
-      bot.on("callback_query", (query) => {
-        const chatId = query.message.chat.id;
-        const quizId = query.message.message_id;
-        const userId = query.from.id;
-        const command = query.data;
-        bot.sendMessage(chatId, "Пипупа")
-        let userState = userStates.get(userId);
-      
-        if (userState.isQueryProcessed) {
-          return; // Игнорировать повторные вызовы
-        }
-      
-        console.log("comand", command, "right", userState);
-      
-        if (command === userState.rightAnswer) {
-          bot.sendMessage(chatId, "Правильно").then((sentMessage) => {
-            const messageId = sentMessage.message_id;
-            // sendRightAnswer(userId, word.id);
-      
-            const timeout = setTimeout(() => {
-              bot.deleteMessage(chatId, messageId);
-              bot.deleteMessage(chatId, quizId);
-              clearTimeout(timeout);
-            }, 1000);
-          });
-          userState.isQueryProcessed = true;
-          return;
-        }
-        if (command !== userState.rightAnswer) {
-          bot.sendMessage(chatId, "Не угадало").then((sentMessage) => {
-            userState.isQueryProcessed = true;
-            const messageId = sentMessage.message_id;
-            // sendWrongAnswer(userId, word.id);
-      
-            const timeout = setTimeout(() => {
-              bot.deleteMessage(chatId, messageId);
-              bot.deleteMessage(chatId, quizId);
-      
-              clearTimeout(timeout);
-            }, 1000);
-          });
-          userState.isQueryProcessed = true;
-        }
-      });
   }
+
+  bot.on("callback_query", (query) => {
+    const chatId = query.message.chat.id;
+    const quizId = query.message.message_id;
+    const userId = query.from.id;
+    const command = query.data;
+
+    let userState = userStates.get(userId);
+    
+    if (userState.isQueryProcessed) {
+      return; // Игнорировать повторные вызовы
+    }
+
+    console.log('comand', command, "right", userState);
+
+    if (command === userState.rightAnswer) {
+      bot.sendMessage(chatId, "Правильно").then((sentMessage) => {
+        const messageId = sentMessage.message_id;
+        sendRightAnswer(userId, word.id);
+
+        const timeout = setTimeout(() => {
+          bot.deleteMessage(chatId, messageId);
+          bot.deleteMessage(chatId, quizId);
+          clearTimeout(timeout);
+        }, 1000);
+      });
+      userState.isQueryProcessed = true;
+      return;
+    } 
+     if (command !== userState.rightAnswer) {
+      bot.sendMessage(chatId, "Не угадало").then((sentMessage) => {
+        userState.isQueryProcessed = true;
+        const messageId = sentMessage.message_id;
+        sendWrongAnswer(userId, word.id);
+
+        const timeout = setTimeout(() => {
+          bot.deleteMessage(chatId, messageId);
+          bot.deleteMessage(chatId, quizId);
+
+          clearTimeout(timeout);
+        }, 1000);
+      });
+      userState.isQueryProcessed = true;
+    }
+  });
 };
-
-
 
 router.post("/bot", (ctx) => {
   const { body } = ctx.request;
@@ -190,6 +188,96 @@ bot.on("message", (msg) => {
     bot.deleteMessage(chatId, messageId);
   }
 });
+
+// bot.onText(/\/go/, (msg) => {
+//   const chatId = msg.chat.id.toString();
+//   const messageId = msg.message_id;
+//   bot.deleteMessage(chatId, messageId);
+
+//   let canSendNewMessage = true;
+//   setInterval(() => {
+//     if (canSendNewMessage) {
+//       canSendNewMessage = false;
+//       bot.removeAllListeners("callback_query");
+
+//       (async () => {
+//         const words = await getUserWords(chatId);
+
+//         const userNexts = await getAllUserNext(chatId);
+//         const isHaveWords = words.length ? true : false;
+//         const gates = isHaveWords ? words : userNexts;
+
+//         const translations = await getTranslations();
+//         const randomWordIndex = Math.floor(Math.random() * gates.length);
+//         const currentWord = gates[randomWordIndex];
+//         const showThisWord = await getOneWord(currentWord.wordId);
+
+//         const { word, translation, translationId } = showThisWord;
+//         const filteredTranslations = translations.filter(
+//           (translation) => translation.id !== translationId
+//         );
+//         function compareRandom() {
+//           return Math.random() - 0.5;
+//         }
+//         filteredTranslations.sort(compareRandom);
+//         const answers = [
+//           translation,
+//           filteredTranslations[0].title,
+//           filteredTranslations[1].title,
+//         ];
+//         answers.sort(compareRandom);
+
+//         const options = {
+//           reply_markup: {
+//             inline_keyboard: [
+//               [{ text: answers[0], callback_data: answers[0] }],
+//               [{ text: answers[1], callback_data: answers[1] }],
+//               [{ text: answers[2], callback_data: answers[2] }],
+//             ],
+//           },
+//         };
+
+//         bot.sendMessage(chatId, `Переведи слово: ${word}`, options);
+
+//         bot.on("callback_query", (query) => {
+//           const chatId = query.message.chat.id;
+//           const messageId = query.message.message_id;
+//           const command = query.data;
+//           canSendNewMessage = true;
+//           console.log("query", query);
+
+//           if (command === translation) {
+//             bot.sendMessage(chatId, "Правильно").then((sentMessage) => {
+//               const messageId = sentMessage.message_id;
+
+//               const timeout = setTimeout(() => {
+//                 bot.deleteMessage(chatId, messageId);
+//                 clearTimeout(timeout);
+//               }, 1000);
+
+//               addToNext(currentWord.wordId, chatId);
+//             });
+//           } else {
+//             bot.sendMessage(chatId, "Не угадало").then((sentMessage) => {
+//               const messageId = sentMessage.message_id;
+//               const timeout = setTimeout(() => {
+//                 bot.deleteMessage(chatId, messageId);
+//                 clearTimeout(timeout);
+//               }, 1000);
+//             });
+
+//             //если из Next
+//             if (!isHaveWords) {
+//               addToUserWord(currentWord.wordId, chatId);
+//             }
+//           }
+
+//           bot.deleteMessage(chatId, messageId);
+//         });
+//       })();
+//     }
+//   }, 3000);
+// });
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
