982730fd4d64cdae31d8b50c9b7bf14a22786a18
Cyrill Prue <49589547+kir8mir@users.noreply.github.com>
2023-07-02 14:27:27 +0300

    ct

--- index.js
+++ index.js
@@ -31,6 +31,18 @@ const updateServer = async () => {
   for (const userAction of usersActions) {
     const { userId, fiveRandomTranslations, word } = userAction;
 
+    let userState = userStates.get(userId);
+
+      if (!userState) {
+        // Если состояние пользователя не существует, создаем новый объект состояния
+        userState = {
+          isQueryProcessed: false,
+          rightAnswer: word.translations[0].title,
+          // Другие свойства состояния пользователя
+        };
+        userStates.set(userId, userState);
+      }
+
     const answers = [
       word.translations[0].title,
       fiveRandomTranslations[0].title,
@@ -69,17 +81,7 @@ const updateServer = async () => {
       const command = query.data;
 
 
-      let userState = userStates.get(userId);
-
-      if (!userState) {
-        // Если состояние пользователя не существует, создаем новый объект состояния
-        userState = {
-          isQueryProcessed: false,
-          rightAnswer: word.translations[0].title,
-          // Другие свойства состояния пользователя
-        };
-        userStates.set(userId, userState);
-      }
+      
       if (userState.isQueryProcessed) {
         return; // Игнорировать повторные вызовы
       }
